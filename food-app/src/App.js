8c65fd8c43d7e9140cddef2360f2f3653e7e2418
Milan Miloradovic <milan.miloradovic@cybercom.com>
2019-06-25 16:46:08 +0200

    first commit

---
+++ food-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './App.css';
+
+function App() {
+  return (
+   <div>Hello from our recipe app</div>
+  );
+}
+
+export default App;
