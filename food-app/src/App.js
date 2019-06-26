080c7be8c14b3bdbef7197662f1d5cd8876ad395
Milan Miloradovic <milan.miloradovic@cybercom.com>
2019-06-26 14:01:56 +0200

    navbar, search

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home'
+import Recipes from './pages/Recipes'
+import SingleRecipe from './pages/SingleRecipe'
+import Default from './pages/Default';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-   <div>Hello from our recipe app</div>
-  );
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <main>
+          {/* navbar */}
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" component={Recipes} />
+            <Route path="/recipes:id" component={SingleRecipe} />
+            <Route component={Default} />
+          </Switch>
+        </main>
+
+      </Router>
+
+    );
+  }
 }
 
 export default App;
